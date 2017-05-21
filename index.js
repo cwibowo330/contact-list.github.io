cd4908f3d82b20a81d8c10583281778541915bd5
Claudia Wibowo <claudiawibowo@gmail.com>
2017-05-20 23:56:42 -0700

    initial

---
+++ index.js
@@ -0,0 +1,80 @@
+(function (){
+	function changeIntoArr(arr) {
+		var newArr = [];
+		for (var a = 0; a < arr.length; a++) {
+			newArr.push(arr[a]);
+		}
+		return newArr;
+	}
+	function grabParent(e){
+		console.log(e);
+		return e.target.parentNode;
+	}
+
+	var allNames = changeIntoArr(document.querySelectorAll('td.name'))
+	var selectItem = document.querySelector('#preference');
+	var emailInfos = changeIntoArr(document.querySelectorAll('span.email-info'));
+	var phoneInfos = changeIntoArr(document.querySelectorAll('span.phone-info'));
+
+	if(navigator.appVersion.indexOf("MSIE 7.")!=-1 || navigator.appVersion.indexOf("MSIE 8.") != -1 || navigator.appVersion.indexOf("MSIE 9.") != -1) {
+		// highlights selected name and displays info
+		for(var i = 0; i < allNames.length; i++) {
+			var parentItem;
+			allNames[i].attachEvent('onmouseenter', function(e){
+				parentItem = grabParent(e);
+				parentItem.className += 'highlight';
+			});
+			allNames[i].attachEvent('onmouseenter', function(e){
+				parentItem.className = '';
+			});
+		}
+
+		// select switches between email or phone number
+		selectItem.attachEvent('onchange', function(){
+			if (selectItem.value == 'email') {
+				for (var e = 0; e < emailInfos.length; e++) {
+					phoneInfos[e].className = 'phone-info';
+					emailInfos[e].className += 'active';
+				}
+				switchInfo(selectItem.value);
+			} else if (selectItem.value == 'phone'){
+				for (var p = 0; p < phoneInfos.length; p++) {
+					emailInfos[p].className = 'email-info';
+					phoneInfos[p].className += 'active';
+				}
+			}
+		})
+	} else {
+		// highlights selected name and displays info
+		for(var i = 0; i < allNames.length; i++) {
+			var parentItem;
+			allNames[i].addEventListener('mouseenter', function(e){
+				parentItem = grabParent(e);
+				parentItem.classList.add('highlight');
+			});
+			allNames[i].addEventListener('mouseleave', function(e){
+				parentItem.classList.remove('highlight');
+			});
+		}
+		
+		// select switches between email or phone number
+		selectItem.addEventListener('change', function(){
+			if (selectItem.value == 'email') {
+				for (var e = 0; e < emailInfos.length; e++) {
+					phoneInfos[e].classList.remove('active');
+					emailInfos[e].classList.add('active');
+				}
+				switchInfo(selectItem.value);
+			} else if (selectItem.value == 'phone'){
+				for (var p = 0; p < phoneInfos.length; p++) {
+					emailInfos[p].classList.remove('active');
+					phoneInfos[p].classList.add('active');
+				}
+			}
+		})
+	}
+
+	
+	
+	
+})();
